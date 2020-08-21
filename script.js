165015513e62a3e29fe37b834da4f6ecedee4400
Dee <56422634+davlsb@users.noreply.github.com>
2020-08-21 19:35:19 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+ AOS.init({
+        offset: 100, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 1000 // values from 0 to 3000, with step 50ms
+      });
