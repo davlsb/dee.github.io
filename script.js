8c2c6c32e02ba93907c103239209bf02e1a367de
Dee <56422634+davlsb@users.noreply.github.com>
2020-08-24 18:54:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
- AOS.init({
+ //Animations
+AOS.init({
         offset: 100, // offset (in px) from the original trigger point
         delay: 0, // values from 0 to 3000, with step 50ms
         duration: 1000 // values from 0 to 3000, with step 50ms
       });
+
+//scrollBar
+  window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
