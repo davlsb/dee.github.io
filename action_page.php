268eae294c8bfe744582aead7ed80e46e2536aef
Dee <56422634+davlsb@users.noreply.github.com>
2020-10-30 16:17:43 -0400

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,15 +1,3 @@
-<html>
-<head>
-<title>Confirmation Page of Web Form</title>
-</head>
-<h1>Confirmation Page of Customer Info</h1>
-
-<p>Thank you for submitting this form. 
-
-<p>We have not received it, as the form isn't working right now.
-
-<p>Below is a summary of the information you provided.<br><br>  
-<?php
-echo 'First Name: ' . $_POST ["name"] . '<br>';
-echo 'Email: ' . $_POST ["email"] . '<br>';
-?>
+Null
+Server not running
+Form not working
